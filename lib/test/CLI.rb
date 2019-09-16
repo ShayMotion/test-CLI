aba2ecbce2b51a461016d58f1d14633f5004eefc
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-09-16 16:17:34 +0000

    first commit

---
+++ lib/test/CLI.rb
@@ -0,0 +1,8 @@
+require "test/CLI/version"
+
+module Test
+  module CLI
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/test/CLI/version.rb
@@ -0,0 +1,5 @@
+module Test
+  module CLI
+    VERSION = "0.1.0"
+  end
+end
